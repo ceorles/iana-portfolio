7d081c503fbd014b3e9d89175a23592d6d3aeca9
Drei <berches.avrles09@gmail.com>
2026-09-23 03:17:32 +0800

    Initial Commit

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from './Footer.module.css'
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>© 2026 | Programmed by ceorles</p>
+      <p>© 2026 | Programmed by iana.</p>
     </footer>
   )
 }
